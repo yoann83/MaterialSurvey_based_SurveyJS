2145a67cca9235bc21184c234d980688a5b2021a
Martinez yoann <41195157+yoann83@users.noreply.github.com>
2022-06-15 11:53:06 +0200

    update poc

--- src/App.js
+++ src/App.js
@@ -3,28 +3,79 @@ import Survey from "material-survey/components/Survey";
 import "survey-core/modern.min.css";
 
 export default function App() {
+  //surchage reactNatif
+  const validateForm = (value) => {
+    if (value.Services.length > 0) {
+      alert(JSON.stringify(value));
+    } else {
+      alert(JSON.stringify("champ Services est vide"));
+    }
+  };
+
   return (
     <>
       <Survey
         form={{
           questions: [
             {
-              name: "multiline-text-example",
-              title: "Do you think we should legalize mayonaise?",
+              name: "company",
+              type: "text",
+              title: "Société :",
+              isRequired: true
+            },
+            {
+              name: "leader",
+              type: "text",
+              title: "Dirigeant :"
+            },
+            {
+              name: "employees",
+              title: "Nom des salariés :",
               type: "multiline-text"
             },
             {
-              max: 10,
+              max: 50,
               min: 0,
-              name: "slider-example",
-              step: 0.5,
-              title: "What was your GPA in middle school?",
+              name: "partnership",
+              step: 1,
+              title: "Nombre de partenariat",
               type: "slider"
+            },
+            {
+              choices: [
+                { text: "Designer", value: "designer" },
+                { text: "Développement", value: "dev" },
+                { text: "Management", value: "management" },
+                { text: "Marketing", value: "trade" },
+                { text: "Ressource Humaine", value: "rh" },
+                { text: "Système-réseau", value: "networks" },
+                { text: "Autres", value: "others" }
+              ],
+              name: "Services",
+              title: "Services :",
+              type: "multiple-dropdown"
+            },
+            {
+              isRequired: true,
+              name: "text-feeling",
+              title: "Années d'activité : ",
+              placeholder: "5",
+              type: "text",
+              validators: [
+                {
+                  maxValue: 20,
+                  minValue: 1,
+                  text: "Nombre doit être compris entre 0 et 20",
+                  type: "numeric"
+                }
+              ]
             }
           ]
         }}
+        onFinish={(answers) => {
+          validateForm(answers);
+        }}
       />
-      ;
     </>
   );
 }
